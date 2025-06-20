e6955c5caadbb5f852ada6f51bcfd3c81fe72eba
mhmmddzaky <muh.dzaky1919@gmail.com>
2025-06-20 21:39:49 +0700

    pembaruan

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # 3. MLflow setup
-mlflow.set_experiment("Penguins-Klasifikasi")
+mlflow.set_experiment("penguins-klasifikasi")
 # Set more conservative autologging to avoid version conflicts
 mlflow.sklearn.autolog(
     log_input_examples=True,
